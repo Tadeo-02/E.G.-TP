463097ffd2987e5a5a7c2eb27d9c21574350eef8
Tadeo-02 <tadeorufine02@gmail.com>
2025-01-12 23:47:07 -0300

    Archivo login PHP y modificaciones al form en el  login. html
    Codigo del login en PHP
    Ajuste al form del archivo html del login para que funcione con el php
    Archivos temporales (eureka y fail) para testear login
    - Solucionando problemas de conexion

---
+++ Page/login.php
@@ -0,0 +1,44 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST)"){
+        
+        // saco la data del formulario
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        // Conexion con la DB
+
+        $host = "localhost";
+        $dbemail = "root";
+        $dbpassword = "";
+        $dbname = "auth";
+
+        $conn = new mysqli($host, $dbemail, $dbpassword, $dbname);
+        
+        if($conn->connect_error){
+            die("Falló la conexión :v " . $conn->connect_error);
+        }
+
+        // Consulta a la DB: validar login y password
+        $query = "SELECT *FROM login WHERE email = '$email' AND password = '$password'";
+
+        $result = $conn->query($query);
+
+        if($result->num_rows == 1){
+            // Login exitoso
+            // echo "Bienvenido";
+            header("Location: eureka.html");
+            exit();  
+
+        }else{          
+            // Login fallido
+                echo "Usuario o contraseña incorrectos";
+                header("Location: fail.html");
+                exit();
+            }
+
+        $conn->close();
+
+
+    }
+
+?>
