b28293ea1d15d06b6156c5aa9b3b54a3af65dd34
Matias <morici.mati@gmail.com>
2025-01-30 02:36:31 -0300

    Frontend listado de promociones microwaved
    - Faltan pequeños ajustes en la simetria del listado

--- Page/inc/footer.php
+++ Page/inc/footer.php
@@ -49,6 +49,6 @@
 
 <footer class="bg-dark p-2 text-center">
     <div class="container">
-        <p class="text-white">Todos los derechos reservados</p>
+        <p class="text" style="color: #ffca2c">Todos los derechos reservados</p>
     </div>
 </footer>
--- Page/vistas/promocionesList.php
+++ Page/vistas/promocionesList.php
@@ -27,60 +27,53 @@
                 $diaHastaActual = isset($_POST['diaHasta']) ? $_POST['diaHasta'] : '';
             ?>
 
-            <div class="row  justify-content-center align-items-center calendarios">
+            <div class="row calendarios">
                 
                 <!-- Formulario con un desplegable -->
-                <div class="col-auto">
+                <div class="columnaFiltro">
                     <div class="calendarContainer">
                         <p>Selecciona una fecha</p>
                         <form action="" method="POST">
                             <label for="diaDesde">Fecha Inicio:</label>
                             <input type="date" id="diaDesde" name="diaDesde">
                             <br>
-
+                            <br>
                             <label for="diaHasta">Fecha Fin:</label>
                             <input type="date" id="diaHasta" name="diaHasta">
                             <br>
 
                             <button type="submit">Enviar</button>
                         </form>
-
                     </div>
-
                 </div>
 
+                <div class="columnaFiltro">
+                    <?php
+                        // Cerrar la conexión
+                        mysqli_close($conexion);
+                        
+                        $diaDesde = isset($_POST['diaDesde']) ? $_POST['diaDesde'] : '';
+                        $diaHasta = isset($_POST['diaHasta']) ? $_POST['diaHasta'] : '';
 
-                <?php
-                    // Cerrar la conexión
-                    mysqli_close($conexion);
-                    
-                ?>
+                        if(!isset($_GET['page'])){
+                            $pagina=1;
+                        }else{
+                            $pagina=(int) $_GET['page'];
+                            if($pagina<=1){
+                                $pagina=1;
+                            }
+                        };
 
+                        $pagina=limpiar_cadena($pagina);
+                        $url="index.php?vista=promocionesList&diaDesde=$diaDesde&diaHasta=$diaHasta&page="; 
+                        $registros=3;
+
+                        # Paginador locales #
+                        require_once (__DIR__. '/../php/listaPromociones.php');
+
+                    ?>
+                </div>
             </div>
-        </div>
-
-        <div class="container">
-            <?php
-                $diaDesde = isset($_POST['diaDesde']) ? $_POST['diaDesde'] : '';
-                $diaHasta = isset($_POST['diaHasta']) ? $_POST['diaHasta'] : '';
-
-                if(!isset($_GET['page'])){
-                    $pagina=1;
-                }else{
-                    $pagina=(int) $_GET['page'];
-                    if($pagina<=1){
-                        $pagina=1;
-                    }
-                };
-
-                $pagina=limpiar_cadena($pagina);
-                $url="index.php?vista=promocionesList&diaDesde=$diaDesde&diaHasta=$diaHasta&page="; 
-                $registros=3;
-
-                # Paginador locales #
-                require_once (__DIR__. '/../php/listaPromociones.php');
-
-            ?>
         </div>
     </div>
 </div>
